52d8b5ef54413a6b822f6ef01f5a3c2e3e2d6e63
inanBekir <bbekirinan@gmail.com>
2020-02-11 08:36:40 +0100

    authentication added

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+    skip_before_action :authenticate_request, only: %i[login register]
+
+    def login
+        authenticate params[:email], params[:password]
+      end
+      def test
+        render json: {
+              message: 'You have passed authentication and authorization test'
+            }
+      end
+    # POST /register
+  def register
+    @user = User.create(user_params)
+   if @user.save
+    render json: @user, status: 201 
+   else
+    render json: @user.errors, status: 422
+   end 
+  end
+
+  private
+  def authenticate(email, password)
+    command = AuthenticateUser.call(email, password)
+
+    if command.success?
+      render json: {
+        access_token: command.result,
+        message: 'Login Successful'
+      }
+    else
+      render json: { error: command.errors }, status: :unauthorized
+    end
+end
+
+  def user_params
+    params.permit(
+      :name,
+      :email,
+      :password
+    )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'urails/s'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'auth/register', to: 'users#register'
+  post 'auth/login', to: 'users#login'
+  get 'test', to: 'users#test'
 end
